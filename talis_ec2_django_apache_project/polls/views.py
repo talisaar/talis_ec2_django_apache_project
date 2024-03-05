3ba3f183f939e7afbb4b3fe56f4336185d6f0165
talisaar <tali.saar@gmail.com>
2024-03-05 14:06:30 +0200

    ready for initial deployment!

--- talis_ec2_django_apache_project/polls/views.py
+++ talis_ec2_django_apache_project/polls/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s. POOP" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s POOP."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s POOP." % question_id)
