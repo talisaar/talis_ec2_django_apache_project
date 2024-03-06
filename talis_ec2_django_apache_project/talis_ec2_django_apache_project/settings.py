e89d46b5183336469615ad7219ceb4cfadf78b3d
talisaar <tali.saar@gmail.com>
2024-03-06 13:46:04 +0200

    remnames static_root, media_root

--- talis_ec2_django_apache_project/talis_ec2_django_apache_project/settings.py
+++ talis_ec2_django_apache_project/talis_ec2_django_apache_project/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = str(BASE_DIR) + "/static_root"
-MEDIA_ROOT = str(BASE_DIR) + "/media_root"
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # stackoverflow said must have same name as static url!
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
