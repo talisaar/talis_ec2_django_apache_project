43e8edcab887edc88c2e8f615d5b6b160f0b7a2a
talisaar <tali.saar@gmail.com>
2024-03-06 13:38:54 +0200

    make basedir to str so prod can read it

--- talis_ec2_django_apache_project/talis_ec2_django_apache_project/settings.py
+++ talis_ec2_django_apache_project/talis_ec2_django_apache_project/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR + "/static_root"
-MEDIA_ROOT = BASE_DIR + "/media_root"
+STATIC_ROOT = str(BASE_DIR) + "/static_root"
+MEDIA_ROOT = str(BASE_DIR) + "/media_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
