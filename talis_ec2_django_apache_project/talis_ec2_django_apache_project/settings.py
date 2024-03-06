289094f648b672cf45990b9d15c0d965d05ac2e1
talisaar <tali.saar@gmail.com>
2024-03-06 13:22:46 +0200

    added static_root and media_root

--- talis_ec2_django_apache_project/talis_ec2_django_apache_project/settings.py
+++ talis_ec2_django_apache_project/talis_ec2_django_apache_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR + "/static_root"
+MEDIA_ROOT = BASE_DIR + "/media_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
